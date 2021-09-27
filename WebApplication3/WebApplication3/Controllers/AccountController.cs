fa2ae535ff4e989fb024d3036e02f073a93e1ad1
Andrew Jarek <andrewjarek@gmail.com>
2021-09-26 19:18:11 -0500

    Initial Commit

---
+++ WebApplication3/WebApplication3/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Okta.AspNetCore;
+
+namespace WebApplication3.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            if (!HttpContext.User.Identity.IsAuthenticated)
+            {
+                return Challenge(OktaDefaults.MvcAuthenticationScheme);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [Authorize]
+        public IActionResult Protected()
+        {
+            // Only for signed-in users!
+            return View();
+        }
+
+    }
+}
